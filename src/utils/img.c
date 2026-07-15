704ac3cdcfdf6c3eeedbb959ec897932c11a0235
joe42-dev <joachim.jeanneau@gmail.com>
2026-07-15 11:49:13 +0200

    fix merge 2

--- src/utils/img.c
+++ src/utils/img.c
@@ -12,12 +12,6 @@
 
 #include "cub3d.h"
 
-/*
-** Ecrit un pixel dans le buffer image (jamais mlx_pixel_put en boucle).
-** Le garde x/y est OBLIGATOIRE : addr est un char* brut, un offset hors
-** image corromprait le tas. line_len (pas WIN_W*4) car la ligne peut etre
-** padee. *(unsigned int*) suppose bpp == 32 (cas standard 42).
-*/
 void	my_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
