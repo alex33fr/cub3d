39689957a474ebbcc8c31ca4d94cef0208f5e2a2
Joachim Jeanneau <jjeannea@1C4.42angouleme.fr>
2026-07-15 15:56:26 +0200

    modif main supp debug printf

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jjeannea <jjeannea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 11:22:31 by jjeannea          #+#    #+#             */
-/*   Updated: 2026/07/15 14:58:16 by jjeannea         ###   ########.fr       */
+/*   Updated: 2026/07/15 15:56:02 by jjeannea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,7 @@ static	int load_colors(t_game *g, char *path)
 	if (!ft_parsing(path, &data))
 		return (1);
 	g->floor = rgb_to_int(data.color_f[0], data.color_f[1], data.color_f[2]);
-	printf("floor = %d, %d, %d\n", data.color_f[0], data.color_f[1], data.color_f[2]);
 	g->ceiling = rgb_to_int(data.color_c[0], data.color_c[1], data.color_c[2]);
-	printf("floor = %d, %d, %d\n", data.color_c[0], data.color_c[1], data.color_c[2]);
 
 	return (0);
 }
